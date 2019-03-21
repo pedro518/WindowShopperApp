72c2148f2062314e37ede1959a14fb1662528ecd
Pedro Antonio Ruiz Cuesta <pruiz@Pedros-iMac.local>
2019-03-21 12:19:24 -0700

    Se crea UiCurrencyTxtField.swift y se utiliza en el VC

---
+++ WindowShopeerApp/WindowShopeerApp/view/UICurrencyTxtField.swift
@@ -0,0 +1,30 @@
+//
+//  UICurrencyTxtField.swift
+//  WindowShopeerApp
+//
+//  Created by Pedro Antonio Ruiz Cuesta on 21/03/2019.
+//  Copyright © 2019 Pedro Antonio Ruiz Cuesta. All rights reserved.
+//
+
+import UIKit
+
+class UICurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.7016395246)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if placeholder == nil {
+            placeholder = ""
+        }
+        
+        let place = NSAttributedString(string: placeholder!, attributes: [.foregroundColor: UIColor.white])
+        attributedPlaceholder = place
+        
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        
+    }
+
+}
