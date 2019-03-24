ff0152b96072b867d114ba5fbaaaf4c2dbe199aa
Pedro Antonio Ruiz Cuesta <pruiz@Pedros-iMac.local>
2019-03-24 05:14:11 -0700

    Se añade simbolo de moneda a la custom view que creamos anteriormenteç

--- WindowShopeerApp/WindowShopeerApp/view/UICurrencyTxtField.swift
+++ WindowShopeerApp/WindowShopeerApp/view/UICurrencyTxtField.swift
@@ -10,6 +10,22 @@ import UIKit
 
 @IBDesignable
 class UICurrencyTxtField: UITextField {
+    
+    override func draw(_ rect: CGRect) {
+        let size : CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.1419203368, green: 0.1419203368, blue: 0.1419203368, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        
+        addSubview(currencyLbl)
+    }
 
     override func prepareForInterfaceBuilder() {
         customizeView()
@@ -25,6 +41,7 @@ class UICurrencyTxtField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3001485475)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
         
         if placeholder == nil {
             placeholder = " "
